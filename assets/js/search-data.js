cdff55d316db1c08dd4e5f66ee89d3f2238434f1
himal2007 <stha.himal2007@gmail.com>
2025-03-29 00:44:52 +0000

    Deploying to gh-pages from @ himal2007/himal2007.github.io@6653b30e8eb59d02acfa72efaea9afefc863012b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-testing-changes-in-your-python-packages-with-github-actions-workflow",
+        },{id: "post-",
+      
+        title: "",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/2025-03-26-python-functions-vs-classes/";
+        
+      },
+    },{id: "post-testing-changes-in-your-python-packages-with-github-actions-workflow",
       
         title: "Testing changes in your Python Packages with GitHub Actions Workflow",
       
